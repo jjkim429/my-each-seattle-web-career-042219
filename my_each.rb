78b940f4d5baccca6ee85822d1081d043704abc4
Jennifer Kim <jjkim429@gmail.com>
2019-04-10 02:53:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,7 @@
-def my_each # put argument(s) here
-  # code here
-end
-
-def hello_t(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
-  else
-    puts "Hey! No block was given!"
+def my_each(array)
+  while i < array.length
+    yield(array[i])
+    i = i += 1
   end
+  array
 end
